ac94f0768c015a2fdde2dea3a0f60011906085a3
hongquyen1910 <150494634+hongquyen1910@users.noreply.github.com>
2023-12-17 10:17:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,13 @@ document.querySelector('#timkiem').onclick = () => {
             },
         }
       });
-
+//-------------------------------------CARTEGORY-------------------
+const itemSlidebar = document.querySelectorAll(".cartegory-left-li")
+itemSlidebar.forEach(function(menu,index){
+    menu.addEventListener("click",function(){
+        menu.classList.toggle("block")
+    })
+})
 //------------------------------------PRODUCT-------------------
 const bigImg = document.querySelector(".product-content-left-big-img img")
 const smallImg = document.querySelectorAll(".product-content-left-small-img img")
@@ -88,18 +94,18 @@ smallImg.forEach(function(imgItem,X){
     })
 })
 
-const baoQuan = document.querySelector(".baoquan")
+const baoQuan = document.querySelector(".danhgia")
 const chiTiet = document.querySelector(".chitiet")
 if(baoQuan){
     baoQuan.addEventListener("click",function(){
         document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"    
-        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-danhgia").style.display = "block"
 })
 }
 if(chiTiet){
     chiTiet.addEventListener("click",function(){
         document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"    
-        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-danhgia").style.display = "none"
             })
 }
 

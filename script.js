b2321b8d38cd07443ea94764d1ca5ec680de4f15
lethinhuy-72 <150515330+lethinhuy-72@users.noreply.github.com>
2023-12-18 17:51:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 
 
 //------------------------------------THU GỌN MENU ------------------- 
-let menu = document.querySelector('#menu');
-let navbar = document.querySelector('.navbar');
-
+let menu = document.querySelector('#menu'); //tham chiếu phần tử có id  là menu
+let navbar = document.querySelector('.navbar'); // tương tự
+// khi phần tử có id trên được click vào, thì giá trị của lớp ('') được thêm hoặc loại bỏ. 
 menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+  menu.classList.toggle('fa-times'); //để thay đổi hình ảnh hoặc biểu tượng của một nút khi trạng thái của trang web thay đổi.
+  navbar.classList.toggle('active'); //để hiển thị hoặc ẩn menu điều hướng (navbar) khi trạng thái của trang web thay đổi.
 }
 //------------------------------------THANH TÌM KIẾM------------------- 
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#timkiem').onclick = () => {
-  searchForm.classList.toggle('active');
+  searchForm.classList.toggle('active'); //để hiển thị hoặc ẩn ô tìm kiếm khi được click vào icon được gán id là timkiem.
 };
 
 
     
  //------------------------------------SLIDE TRANG CHỦ -------------------    
+      // tạo đối tượng swiper để tạo ra hiệu ứng hoạt ảnh
     var swiper = new Swiper(".trangchu-slider", {
       spaceBetween: 30,
       centeredSlides: true,
@@ -24,12 +25,14 @@ document.querySelector('#timkiem').onclick = () => {
         delay: 7500,
         disableOnInteraction: false
       },
-      pagination: {
+      // khoảng cách giữa các slide là 30px,, dùng centeredSlide để hiển thị slide được chọn nằm ở giữa vùng slider, thời gian giữa các chuyển động là 0.5s, giữ tự động chuyển slide bằng khi slider được hiển thị bằng lệnh disableOnInteraction
+       pagination: {
         el: ".swiper-pagination",
         clickable: true,
-      },
+      }, // Tạo nút điều hướng trang và cho phép người dùng click vào nút trang để di chuyển đến slide tương ứng
       loop:true,
-    });
+    }); // cho phép lặp vô hạn giữa các slide
+
 //------------------------------------SLIDE ĐÁNH GIÁ ------------------- 
     var swiper = new Swiper(".danhgia-slider", {
         spaceBetween: 20,
@@ -53,7 +56,7 @@ document.querySelector('#timkiem').onclick = () => {
                 slidesPerView:3,
             },
         }
-      });
+      }); // sử dụng breakpoints để xác định số lượng slides hiển thị ở các kích thước màn hình khác nhau, slidesPerview cho phép xác định số lượng slides hiển thị tại kích thước màn hình tương ứng, từ 0-639px là 1 slide mỗi trang,..
 //------------------------------------SLIDE ẢNH TRANG WORKSHOP -------------------       
       var swiper = new Swiper(".ws-slider", {
         spaceBetween: 0,
@@ -84,7 +87,7 @@ itemSlidebar.forEach(function(menu,index){
     menu.addEventListener("click",function(){
         menu.classList.toggle("block")
     })
-})
+}) // 
 //------------------------------------PRODUCT-------------------
 const bigImg = document.querySelector(".product-content-left-big-img img")
 const smallImg = document.querySelectorAll(".product-content-left-small-img img")
